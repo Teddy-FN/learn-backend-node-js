cb2ced1c6a493c45d9a1497941162e3c3aa9ddd6
Teddy-FN <tedd.ferdy@gmail.com>
2023-09-29 10:01:58 +0700

    add sequelize package

--- App.js
+++ App.js
@@ -21,8 +21,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(home);
 
 // Error Page
-app.use((req, res) => {
-  res.status(404).send("<h1>NOT FOUND!!</h1>");
+app.use((req, res, next) => {
+  res.status(404).render("404", {
+    pageTitle: "404 Not Found !",
+  });
 });
 // server
 app.listen(4000);
